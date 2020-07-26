0436ed35ee91c0218c931db798d8ccf7c07e23c4
Ruben <ih09592p5@gmail.com>
2020-07-26 13:48:39 +0200

    initialization

--- snake.py
+++ snake.py
@@ -1,14 +1,35 @@
 import curses
 # To get curses on Windows: pip install windows-curses
 
-# window setup
-curses.initscr()
-win = curses.newwin(20, 20, 0, 0) # window size; return null if window > terminal
-win.border(0)
-win.keypad(1) # arrow keys to control the snake
-win.nodelay(1) # loop until new input
-curses.noecho() # no other input keys
-curses.curs_set(0) # invisible cursor
+def main(stdscr):
+    stdscr.clear()
 
-# end
-curses.endwin()
+    # Window setup
+    win = curses.newwin(20, 50, 0, 0) # window size - return null if window > terminal
+    win.border(0)
+    win.keypad(1) # arrow keys to control the snake
+    win.nodelay(1) # loop until new input
+    #curses.noecho() # no other input keys
+    curses.curs_set(0) # invisible cursor
+
+    snake = [(3,3), (3,2), (3,1)]
+    item = (10, 10)
+
+    win.addch(item[0], item[1], "#")
+
+    ESC = 27
+    event = win.getch()
+
+    while event != ESC:
+        event = win.getch()
+
+        for c in snake:
+            win.addch(c[0], c[1], "O")
+        
+        win.addch(item[0], item[1], "#")
+    
+    # End
+    # win.refresh()
+    # win.getkey()
+
+curses.wrapper(main)
